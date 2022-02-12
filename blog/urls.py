b3a631ad6b4c878d86b36fd4dd2e1b9236d29740
RilaKorila <murakami.ayana3@gmail.com>
2022-02-12 10:45:16 +0900

    add detail page

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     ]
+# <int:pk>: Django expects an int value and 
+# will transfer it to a view as a variable called pk(Primary Key)
