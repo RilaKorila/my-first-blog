5f833562b42e8237036fde6f161752ad2ccfb4ea
RilaKorila <murakami.ayana3@gmail.com>
2022-02-12 22:57:11 +0900

    add edit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-from re import A
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
@@ -33,3 +32,24 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_new.html', {'form':form})
+
+# give pk values from urls.py
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    
+    # after an user saved the post
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            
+            return redirect('post_detail', pk=post.pk)
+        
+    else:
+        form = PostForm(instance=post)
+    
+    return render(request, 'blog/post_edit.html', {'form': form})
